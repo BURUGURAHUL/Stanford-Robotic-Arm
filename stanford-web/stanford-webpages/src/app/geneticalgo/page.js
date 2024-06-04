33e83853289a2fde5de9cc3d7e902b12450a58c0
amri-tah <amrithanandini2003@gmail.com>
2024-06-04 14:37:00 +0530

    ppt added

--- stanford-web/stanford-webpages/src/app/geneticalgo/page.js
+++ stanford-web/stanford-webpages/src/app/geneticalgo/page.js
@@ -104,9 +104,9 @@ end Function
           {`
 function score = fitness(individual, chromo_size, theta_10, theta_1f, theta_20, theta_2f, d_30, d_3f, tf)
     
-bits = individual;
-[t2, t3] = decode(bits, chromo_size, tf);
-score = distance(theta_10, theta_1f, theta_20, theta_2f, d_30, d_3f, t2, t3, tf);
+    bits = individual;
+    [t2, t3] = decode(bits, chromo_size, tf);
+    score = distance(theta_10, theta_1f, theta_20, theta_2f, d_30, d_3f, t2, t3, tf);
 end
           `}
         </code>
